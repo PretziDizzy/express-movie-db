67dd451de06bd36359f09e0f03ad9d50eae4d4f0
Preeti Das <preetidas_a@yahoo.com>
2021-12-10 02:03:47 -0600

    changed width

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 
-// app.get("/now", (request, response) => {
-//     const date = new Date();
-//     console.log(response.baseUrl + date)
-//     // console.log(response.location)
-//     response.send(date);
-// });
+app.get("/now", (request, response) => {
+    const date = new Date();
+    console.log(response.baseUrl + date)
+    // console.log(response.location)
+    response.send(date);
+});
 
-    app.get('/crew', async (req,res) => {
-        const mycrew = await Crew.findAll()
-        console.log(req)
+    // app.get('/crew', async (req,res) => {
+    //     const mycrew = await Crew.findAll()
+    //     console.log(req)
 
-        res.json(mycrew);
-    })
+    //     res.json(mycrew);
+    // })
 
-    app.get('/cast/:id', async (req,res) => {
-        const thecast = await Cast.findByPk(req.params.id)
-        console.log(req)
+    // app.get('/cast/:id', async (req,res) => {
+    //     const thecast = await Cast.findByPk(req.params.id)
+    //     console.log(req)
 
-        res.json(thecast)
-    })
+    //     res.json(thecast)
+    // })
 
     // app.get('/casts/:castName', async (req,res) => {
     //     const mycast = await Cast.findOne({where: {castName: req.params.name }})
